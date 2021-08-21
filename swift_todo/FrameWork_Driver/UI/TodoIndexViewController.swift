35d918f41c844add955fef177f5cc683e42872e4
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-08-21 20:48:32 +0900

    [Add] Setting

--- swift_todo/FrameWork_Driver/UI/TodoIndexViewController.swift
+++ swift_todo/FrameWork_Driver/UI/TodoIndexViewController.swift
@@ -5,4 +5,11 @@
 //  Created by tk84 on 2021/08/21.
 //
 
-import Foundation
+import UIKit
+
+class TodoIndexViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
--- swift_todo/TodoIndexViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TodoIndexViewController.swift
-//  swift_todo
-//
-//  Created by tk84 on 2021/08/21.
-//
-
-import Foundation
